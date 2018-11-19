2f1dd6ef7cd4fa83aeeaef51d6c02dad90875a4d
Pooya Hatami <am.hatami@gmail.com>
2018-11-19 03:28:21 -0800

    update!

--- IOS-Swift-UIFontTextStyle/ViewController.swift
+++ IOS-Swift-UIFontTextStyle/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var headLineLable: UILabel!
+    @IBOutlet weak var subHeadlineLabel: UILabel!
+    @IBOutlet weak var bodyLabel: UILabel!
+    @IBOutlet weak var footnoteLabel: UILabel!
+    @IBOutlet weak var caption1Label: UILabel!
+    @IBOutlet weak var caption2Label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        updateFonts()
     }
 
 
+    func updateFonts() {
+        headLineLable.font = UIFont.preferredFont(forTextStyle: UIFont.TextStyle.headline)
+        subHeadlineLabel.font = UIFont.preferredFont(forTextStyle: UIFont.TextStyle.subheadline)
+        bodyLabel.font = UIFont.preferredFont(forTextStyle: UIFont.TextStyle.body)
+        footnoteLabel.font = UIFont.preferredFont(forTextStyle: UIFont.TextStyle.footnote)
+        caption1Label.font = UIFont.preferredFont(forTextStyle: UIFont.TextStyle.caption1)
+        caption2Label.font = UIFont.preferredFont(forTextStyle: UIFont.TextStyle.caption2)
+    }
+    
 }
 
